c12d1356473d418d8c0fe210339443fa9f48bc76
joscor34 <zbrm34@gmail.com>
2026-06-17 20:09:07 -0600

    Rutas de la API

---
+++ routes.py
@@ -0,0 +1,13 @@
+# Regla de oro: Todas las rutas que vayamos a crear en el la app irán en este documento
+
+from resources import *
+
+
+class AdministradorDeRutas:
+  # Este método le dará la pauta a mi aplicación de que rutas vamos a poder usar
+  def inicializar_rutas(self, aplicacion):
+    aplicacion.add_resource(PrimerRecurso, '/primero')
+    aplicacion.add_resource(NombreAleatorio, '/segundo')
+    aplicacion.add_resource(PaginaInicio, '/')
+    aplicacion.add_resource(PaginaPreguntas, '/faq')
+    # / -> Ruta Raíz: La primera página que verá el usuario al acceder a nuestra web
